ebcf65a37b4de06f08e0c754866451c6511d969f
mompei <ogunborjerry@gmail.com>
2026-02-10 01:45:18 -0800

    Creating signup request

--- src/controllers/auth.rs
+++ src/controllers/auth.rs
@@ -1,8 +1,17 @@
-use actix_web::{post, Responder};
+use actix_web::{post, web, Responder};
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+struct SignUpRequest {
+    email: String,
+    password: String,
+    firstname: String,
+    lastname: String,
+}
 
 #[post("/auth/sign-up")]
-pub async fn sign_up() -> impl Responder {
-    "Sign Up"
+pub async fn sign_up(data: web::Json<SignUpRequest>) -> impl Responder {
+    format!("Sign Up: {:?}", data)
 }
 
 #[post("/auth/sign-in")]
