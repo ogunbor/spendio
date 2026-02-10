2a7ce00e94c3e8935ead849bc8317a5ed57bd0c2
mompei <ogunborjerry@gmail.com>
2026-02-09 18:14:42 -0800

    users table created and migrated

---
+++ migrations/20260210013453_create_user_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `users` (
+  `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `balance` BIGINT UNSIGNED NOT NULL DEFAULT 0,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+);
