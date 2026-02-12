52873a4d0c2c8b6fa78b1057d8e138fde98e5fbe
mompei <ogunborjerry@gmail.com>
2026-02-11 16:08:50 -0800

    Refactor: get authenticated user

--- src/controllers/me.rs
+++ src/controllers/me.rs
@@ -6,9 +6,7 @@ use crate::{db, utils, AppState};
 #[get("/me")]
 pub async fn get_profile(state: web::Data<AppState>, req: HttpRequest) -> impl Responder {
     let db = state.db.lock().await;
-    let user = db::user::get_by_id(&db, utils::get_user_id(&req))
-        .await
-        .unwrap();
+    let user = utils::get_authenticated_user(&req, &db).await;
 
     HttpResponse::Ok().json(user)
 }
@@ -26,15 +24,11 @@ pub async fn update_profile(
     data: web::Json<UpdateProfileRequest>,
 ) -> impl Responder {
     let db = state.db.lock().await;
-    let user = db::user::get_by_id(&db, utils::get_user_id(&req))
-        .await
-        .unwrap();
+    let user = utils::get_authenticated_user(&req, &db).await;
 
     db::user::update(&db, user.id, &data).await;
 
-    let user = db::user::get_by_id(&db, utils::get_user_id(&req))
-        .await
-        .unwrap();
+    let user = utils::get_authenticated_user(&req, &db).await;
 
     HttpResponse::Ok().json(user)
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,7 +1,13 @@
 use actix_web::{HttpMessage, HttpRequest};
 
+use crate::db;
+
 pub fn get_user_id(req: &HttpRequest) -> u64 {
     let ext = req.extensions();
 
     ext.get::<u64>().unwrap().to_owned()
+}
+
+pub async fn get_authenticated_user(req: &HttpRequest, db: &sqlx::MySqlPool) -> db::user::User {
+    db::user::get_by_id(db, get_user_id(req)).await.unwrap()
 }
