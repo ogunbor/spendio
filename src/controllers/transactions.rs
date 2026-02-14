8b80932fb9e6ebc61fa97140c26970a5fc333705
mompei <ogunborjerry@gmail.com>
2026-02-14 01:22:31 -0800

    Create a transaction

--- src/controllers/transactions.rs
+++ src/controllers/transactions.rs
@@ -1,4 +1,6 @@
 use actix_web::{delete, get, post, put, web, HttpRequest, HttpResponse, Responder};
+use serde::Deserialize;
+use serde_json::json;
 
 use crate::{db, utils, AppState};
 
@@ -12,9 +14,41 @@ pub async fn index(state: web::Data<AppState>, req: HttpRequest) -> impl Respond
     HttpResponse::Ok().json(transactions)
 }
 
+#[derive(Deserialize)]
+pub struct CreateTransactionRequest {
+    pub category_id: u64,
+    pub r#type: String,
+    pub amount: u64,
+    pub memo: String,
+    pub description: Option<String>,
+}
+
 #[post("/transactions")]
-pub async fn create() -> impl Responder {
-    "Transactions: Create"
+pub async fn create(
+    state: web::Data<AppState>,
+    req: HttpRequest,
+    data: web::Json<CreateTransactionRequest>,
+) -> impl Responder {
+    let db = state.db.lock().await;
+    let user_id = utils::get_user_id(&req);
+
+    let Some(category) = db::categories::get(&db, data.category_id).await else {
+        return HttpResponse::NotFound().json(json!({
+            "status": "error",
+            "message": "Category not found"
+        }));
+    };
+
+    if category.user_id != user_id {
+        return HttpResponse::Forbidden().json(json!({
+            "status": "error",
+            "message": "Unauthorized"
+        }));
+    }
+
+    let transaction = db::transactions::create(&db, user_id, &data).await;
+
+    HttpResponse::Created().json(transaction)
 }
 
 #[get("/transactions/{id}")]
--- src/db/transactions.rs
+++ src/db/transactions.rs
@@ -1,5 +1,7 @@
 use serde::Serialize;
 
+use crate::controllers::transactions::CreateTransactionRequest;
+
 #[derive(Serialize)]
 pub struct Transaction {
     id: u64,
@@ -22,4 +24,24 @@ pub async fn get_all_of_user(db: &sqlx::MySqlPool, user_id: u64) -> Vec<Transact
     .fetch_all(db)
     .await
     .unwrap()
+}
+
+pub async fn get(db: &sqlx::MySqlPool, id: u64) -> Option<Transaction> {
+    sqlx::query_as!(Transaction, "SELECT * FROM transactions WHERE id = ?", id)
+        .fetch_optional(db)
+        .await
+        .unwrap()
+}
+
+pub async fn create(
+    db: &sqlx::MySqlPool,
+    user_id: u64,
+    transaction: &CreateTransactionRequest,
+) -> Transaction {
+    let r= sqlx::query!("INSERT INTO transactions (user_id, category_id, type, amount, memo, description) VALUES (?, ?, ?, ?, ?, ?)", user_id, transaction.category_id, transaction.r#type, transaction.amount, transaction.memo, transaction.description)
+        .execute(db)
+        .await
+        .unwrap();
+
+    get(db, r.last_insert_id()).await.unwrap()
 }
